bdc8d94f13a2613050134c5328e54cc944cb146c
Tim <tim@retechlabs.com>
2019-02-14 10:05:32 +0700

    function regex more fix

--- src/providers/pug-url-definition-provider.ts
+++ src/providers/pug-url-definition-provider.ts
@@ -765,7 +765,7 @@ export class PugUrlDefinitionProvider implements vscode.DefinitionProvider {
         return new Promise<vscode.LocationLink>(resolve => {
             vscode.workspace.openTextDocument(link.targetUri)
                 .then(document => {
-                    const functionRegex = new RegExp(`^((?:\\s+)?(?:(?:public|private|protected)(?:\\s+)))${propertyName}(?:\\s+)?\\(([a-zA-Z:\\s,\\n\\r.?]+|)\\)(?:\\s+)?:?[a-zA-Z\\s:]+\\{`, 'gm');
+                    const functionRegex = new RegExp(`^((?:\\s+)?(?:(?:public|private|protected)(?:\\s+))?)${propertyName}(?:\\s+)?\\(([a-zA-Z:\\s,\\n\\r.?$]+|)\\)(?:\\s+)?:?[a-zA-Z\\s:]+\\{`, 'gm');
                     const match = functionRegex.exec(document.getText());
                     if (match) {
                         link.targetRange = new vscode.Range(
